51d9d15b66a51dd0c07ff9f8567254a8a6ded7d2
Aphenphos <MaxwillWinters@gmail.com>
2022-08-23 17:14:27 -0400

    can post a new todo

---
+++ __tests__/todo.test.js
@@ -0,0 +1,47 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const { CookieAccessInfo } = require('cookiejar');
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '123456',
+};
+
+const agent = request.agent(app);
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('can post a new todo', async () => {
+    const signIn = await agent.post('/api/v1/users').send(mockUser);
+    expect(signIn.body).toEqual({
+      email: mockUser.email,
+      password: mockUser.password
+    });
+    const todo = { task: 'make code work' };
+    const res = await agent.post('/api/v1/todos').send(todo);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      task: 'make code work',
+      user_id: expect.any(String),
+      done: false,
+    });
+  });
+
+  it('returns list of todos', async () => {
+    const signIn = await agent.post('/api/v1/users').send(mockUser);
+    expect(signIn.body).toEqual({
+      email: mockUser.email,
+      password: mockUser.password
+    });
+    const res = await agent.get('/api/v1/todos');
+    expect(res.body.length).toBe(0);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
+drop table if exists todos CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR NOT NULL UNIQUE,
   password_hash VARCHAR NOT NULL
 );
+
+create table todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  task text,
+  done boolean default(false),
+  user_id BIGINT not NULL,
+  foreign key (user_id) references users(id)
+)
