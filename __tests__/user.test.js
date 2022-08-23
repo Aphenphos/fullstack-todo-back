09b6311da6693e085751fb5340e3d96ec7707ffc
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-23 12:09:41 -0700

    test passes sign in auth users

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -27,6 +27,19 @@ describe('backend-express-template routes', () => {
     expect(session).toBeTruthy();
   });
 
+  it('signs in existing user', async () => {
+    const create = await agent.post('/api/v1/users').send(mockUser);
+    expect(create.body).toEqual({
+      email: mockUser.email,
+      password: mockUser.password
+    });
+    const resp = await agent.post('/api/v1/users/sessions').send(mockUser);
+    expect(resp.body).toEqual({
+      email: mockUser.email,
+      password: mockUser.password
+    });
+  });
+
   it('returns the current user', async () => {
     const me = await agent.get('/api/v1/users/me');
     console.log(me.body);
@@ -36,6 +49,11 @@ describe('backend-express-template routes', () => {
       exp: expect.any(Number),
       iat: expect.any(Number),
     });
+  });
+
+  it('/get returns a 401 if not logged in', async () => {
+    const resp = await request(app).get('/api/v1/users/me');
+    expect(resp.status).toBe(401);
   });
 
   it('delete user session(logout)', async () => {
