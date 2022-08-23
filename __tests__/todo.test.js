4458dbeb2824f66df9b355102745342337aa6133
Aphenphos <MaxwillWinters@gmail.com>
2022-08-23 18:38:26 -0400

    update

--- __tests__/todo.test.js
+++ __tests__/todo.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { CookieAccessInfo } = require('cookiejar');
 
 const mockUser = {
   email: 'test@example.com',
